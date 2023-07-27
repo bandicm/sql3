475c33651cf1cc092b552b858b7a344ced51900f
marcelb <marcelb@bitelex.ml>
2023-07-27 22:52:22 +0200

    Add mutex as semaphore, for multithread support, also merge with fix-callback

--- test/test.cpp
+++ test/test.cpp
@@ -11,12 +11,12 @@ int main() {
     // cout << mydb.ask("Select * from Tab1");
     // cout << mydb.ask("INSERT INTO Tab1 VALUES(3,'Pakora', 'marijanab@bitelex.ml');");
 
-    // auto res = mydb.query("Select * from Tab1");
-    auto res = mydb.query("INSERT INTO Tab1 VALUES(4,'Mare', 'marijanab@bitelex.co');");
+    auto res = mydb.query("Select * from Tab1");
+    // auto res = mydb.query("INSERT INTO Tab1 VALUES(4,'Mare', 'marijanab@bitelex.co');");
 
-    // for (auto i : res) 
-    //     for (auto j: i.second)
-    //         cout << i.first << " : " << j << endl;
+    for (auto i : res) 
+        for (auto j: i.second)
+            cout << i.first << " : " << j << endl;
     
 
     // cout << endl <<  res["NAME"][1];
